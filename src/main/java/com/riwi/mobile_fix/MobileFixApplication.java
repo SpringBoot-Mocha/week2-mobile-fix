0722dbb1b65fde14838197b82454856d37b60caa
Johan192004 <johanruma@gmail.com>
2025-10-29 21:44:19 -0500

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/riwi/mobile_fix/MobileFixApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.mobile_fix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileFixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/mobile_fix/MobileFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.mobile_fix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
